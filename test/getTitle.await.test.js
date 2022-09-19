15498be45e3ae1204b06ef44a2d05dd920093715
Behram <77900065+ikhanbehram@users.noreply.github.com>
2022-09-19 12:59:49 +0500

    initial testing setup

---
+++ test/getTitle.await.test.js
@@ -0,0 +1,7 @@
+var assert = require("assert");
+
+import { getTitle } from "../controllers/getTitle.await";
+
+describe("get title with await", () => {
+    it("it should look for address", () => {});
+});
